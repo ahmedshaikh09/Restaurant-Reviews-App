653ec0aebece13c7bc4ccc51b9555065cddd67b0
Ahmed Shaikh <shaikhahmed5880@gmail.com>
2019-01-06 16:54:00 -0600

    Getting the Map on screen

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYWhtZWRzaGFpa2gwOSIsImEiOiJjanB5OXcya28wMmFyNDhtenRrcGNkODZiIn0.w9Ej8owNYNuAAxV4RS2rRg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
