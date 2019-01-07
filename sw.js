8d118e8ce044346ef258e13a0432d2f7ed53ecc7
Ahmed Shaikh <shaikhahmed5880@gmail.com>
2019-01-07 11:22:30 -0600

    Files updated

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let staticCache = 'my-cache-1';
+let staticCache = 'resturant-cache-1';
 
 
 self.addEventListener('activate', event => {
@@ -21,25 +21,12 @@ self.addEventListener('install', (event) => {
         caches.open(staticCache).then(function (cache) {
             return cache.addAll(
                 [   
-                    './',
-                    './img/1.jpg',
-                    './img/2.jpg',
-                    './img/3.jpg',
-                    './img/4.jpg',
-                    './img/5.jpg',
-                    './img/6.jpg',
-                    './img/7.jpg',
-                    './img/8.jpg',
-                    './img/9.jpg',
-                    './img/10.jpg',
-                    './js/app.js',
-                    './js/dbhelper.js',
-                    './js/main.js',
-                    './js/resturant_info.js',
-                    './css/styles.css',
-                    './index.html',
-                    './resturant.html'
-
+                    '/',
+                    'img/',
+                    'css/styles.css',
+                    'js/dbhelper.js',
+                    'js/main.js',
+                    'js/restaurant_info.js',
                 ]
             );
         })
